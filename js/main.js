8d8af128047442148489d4f13c7b76022f1f90ea
Sayan Das <42395405+YanDs03@users.noreply.github.com>
2019-02-28 16:26:13 +0530

    Site 1

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var rulesExpandbtn = document.querySelectorAll('#toggle_rule_btn');
+var contactExpandbtn = document.querySelectorAll('#toggle_contact_btn');
+var contact_contents = document.querySelectorAll('.contact_content');
+var rules_contents = document.querySelectorAll('.rules_content');
